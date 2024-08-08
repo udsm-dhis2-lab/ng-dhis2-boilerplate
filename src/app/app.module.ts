e708fd96566fb9afd5c600dac45a4359c113ab3e
Rajab Mkomwa <rajeyw@gmail.com>
2024-08-08 07:30:55 +0300

    feat: add welcome note on landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
-import { AppComponent, AppComponentContent } from './app.component';
 import { AppShellModule } from '@iapps/ng-dhis2-shell';
 import { environment } from '../environments/environment';
+import { AppWrapperComponent } from './app-wrapper.component';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { NgxDhis2HttpClientModule } from '@iapps/ngx-dhis2-http-client';
 
 @NgModule({
   imports: [
@@ -14,8 +16,14 @@ import { environment } from '../environments/environment';
       pwaEnabled: false,
       isDevMode: !environment.production,
     }),
+
+    NgxDhis2HttpClientModule.forRoot({
+      namespace: 'ng-dhis2-boilerplate',
+      version: 1,
+      models: [],
+    }),
   ],
-  declarations: [AppComponent, AppComponentContent],
-  bootstrap: [AppComponent],
+  declarations: [AppComponent, AppWrapperComponent],
+  bootstrap: [AppWrapperComponent],
 })
 export class AppModule {}
