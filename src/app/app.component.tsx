7389f109c02e72c6de5d4cfc238ef20f4bdb2d3a
Rajab Mkomwa <rajeyw@gmail.com>
2025-05-01 11:17:49 +0300

    refactor: update dependencies and organise pages

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -3,49 +3,18 @@ import { NgxDhis2HttpClientService, User } from "@iapps/ngx-dhis2-http-client";
 import React, { useState } from "react";
 import { Observable } from "rxjs";
 import { Chip } from "@dhis2/ui";
+import { D2Window } from "@iapps/d2-web-sdk";
 
 @Component({
-    selector: "app-root-content",
-    templateUrl: "./app.component.html",
-    styleUrls: ["./app.component.scss"],
-    standalone: false
+  selector: "app-root-content",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  standalone: false,
 })
 export class AppComponent {
   private httpClient = inject(NgxDhis2HttpClientService);
   me$: Observable<User> = this.httpClient.me();
   showComponent = signal(false);
-
-  Chips = () => {
-    const [disabled, setDisabled] = useState(false);
-    return (
-      <button
-        disabled={disabled}
-        onClick={() => {
-          setDisabled(!disabled);
-        }}
-      >
-        This is my button
-      </button>
-    );
-  };
-  selectedPeriods = [
-    { id: "THIS_MONTH", name: "This month" },
-    { id: "LAST_MONTH", name: "Last month" },
-    { id: "LAST_3_MONTHS", name: "Last 3 months" },
-  ];
-
-  selectedOrgUnits = [];
-  onSelectPeriods(periods: any) {
-    console.log(periods);
-  }
-
-  onSelectOrgUnits(orgUnits: any) {
-    this.selectedOrgUnits = orgUnits;
-
-    console.log(this.selectedOrgUnits);
-  }
-
-  onToggleComponent() {
-    this.showComponent.set(!this.showComponent());
-  }
+  d2 = (window as unknown as D2Window)?.d2Web;
+  me = this.d2?.currentUser;
 }
